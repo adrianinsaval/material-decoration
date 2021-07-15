ffe833cfceb0f68e05071c11f98db7147a630e13
Adrian <adrianinsaval@gmail.com>
2021-07-15 19:35:41 -0400

    fix borders not updated on activeChanged

--- src/Decoration.cc
+++ src/Decoration.cc
@@ -216,7 +216,7 @@ void Decoration::init()
     auto *decoratedClient = client().toStrongRef().data();
 
     auto repaintTitleBar = [this] {
-        update(titleBar());
+        update();
     };
 
     m_leftButtons = new KDecoration2::DecorationButtonGroup(
